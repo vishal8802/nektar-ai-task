58704dcc49cb01a834cf073cd8063df97d5db0b7
Vishal Singh <svishal8802@gmail.com>
2022-08-31 01:29:29 +0530

    updated error message

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const port = process.env.PORT || 3000;
 app.get('/', async (req, res) => {
   const { startTime, endTime, keyword } = req.query;
   if (startTime && endTime && endTime < startTime)
-    return res.status(400).send('Invalid time range');
+    return res
+      .status(400)
+      .send('Invalid time range, startTime exceeding endTime');
   const logs = await fetchLogs({ startTime, endTime, keyword });
   return res.status(200).send(logs);
 });
