0e0b2584236c526d70e91d59b3be77ff95551478
Vishal Singh <svishal8802@gmail.com>
2022-08-31 01:30:18 +0530

    added try catch

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
-  const { startTime, endTime, keyword } = req.query;
-  if (startTime && endTime && endTime < startTime)
-    return res
-      .status(400)
-      .send('Invalid time range, startTime exceeding endTime');
-  const logs = await fetchLogs({ startTime, endTime, keyword });
-  return res.status(200).send(logs);
+  try {
+    const { startTime, endTime, keyword } = req.query;
+    if (startTime && endTime && endTime < startTime)
+      return res
+        .status(400)
+        .send('Invalid time range, startTime exceeding endTime');
+    const logs = await fetchLogs({ startTime, endTime, keyword });
+    return res.status(200).send(logs);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send('Something went wrong');
+  }
 });
 
 app.listen(port, () => {
